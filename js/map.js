e7e9865b1a54612237fd1d19b61b54bfde55725b
RealRather <36735076+RealRather@users.noreply.github.com>
2018-07-20 23:47:07 +0300

    Добавляет модуль, генерирующий массив объявлений

--- js/map.js
+++ js/map.js
@@ -1,51 +1,15 @@
 'use strict';
 
-var PRICE_MIN_VALUE = 1000;
-var PRICE_MAX_VALUE = 1000000;
-
-var ROOMS_MIN_VALUE = 1;
-var ROOMS_MAX_VALUE = 5;
-
-var GUESTS_MIN_VALUE = 1;
-var GUESTS_MAX_VALUE = 25;
-
-var LOCATION_X_MIN = 300;
-var LOCATION_X_MAX = 900;
-
-var LOCATION_Y_MIN = 130;
-var LOCATION_Y_MAX = 630;
-
 var MAP_PIN_WIDTH = 65;
 var MAP_PIN_HEIGHT = 65;
 
 var MAP_PIN_INITIAL_WIDTH = 50;
 var MAP_PIN_INITIAL_HEIHT = 70;
 
-var AMOUNT_ADVERTS = 8;
+window.LOCATION_Y_MIN = 130;
+window.LOCATION_Y_MAX = 630;
+
 var AUXILIARY_ELEMENTS_COUNT = 2;
-
-var TITLES =
-  [
-    'Большая уютная квартира',
-    'Маленькая неуютная квартира',
-    'Огромный прекрасный дворец',
-    'Маленький ужасный дворец',
-    'Красивый гостевой домик',
-    'Некрасивый негостеприимный домик',
-    'Уютное бунгало далеко от моря',
-    'Неуютное бунгало по колено в воде'
-  ];
-
-var TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var ARRIVAL_TIMES = ['12:00', '13:00', '14:00'];
-var DEPARTURE_TIMES = ['12:00', '13:00', '14:00'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS =
-  [
-    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-  ];
 
 var BUILDING_TYPES = {
   palace: 'Дворец',
@@ -63,59 +27,6 @@ var formInputAddress = document.querySelector('#address');
 var templateMapPin = document.querySelector('template').content.querySelector('.map__pin');
 var templateMapCard = document.querySelector('template').content.querySelector('.map__card');
 var templatePhoto = templateMapCard.querySelector('.popup__photo');
-
-var getRandomElement = function (min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-};
-
-var createDiffArrayLength = function (array) {
-  return array.slice(getRandomElement(0, array.length));
-};
-
-// Перемешивает массив
-var createMixArray = function (array) {
-  var mixArray = function () {
-    return Math.random() - 0.5;
-  };
-  return array.sort(mixArray);
-};
-
-// Генерация объявлений
-var generateAdverts = function (countObjects) {
-  var arrayAdverts = [];
-  for (var i = 0; i < countObjects; i++) {
-    var yPoint = getRandomElement(LOCATION_Y_MIN, LOCATION_Y_MAX);
-    var xPoint = getRandomElement(LOCATION_X_MIN, LOCATION_X_MAX);
-    var objectAdvert = {
-      author: {
-        avatar: 'img/avatars/user0' + (i + 1) + '.png'
-      },
-
-      offer: {
-        title: TITLES[i],
-        address: xPoint + ', ' + yPoint,
-        price: getRandomElement(PRICE_MIN_VALUE, PRICE_MAX_VALUE),
-        type: TYPES[getRandomElement(0, TYPES.length)],
-        rooms: getRandomElement(ROOMS_MIN_VALUE, ROOMS_MAX_VALUE),
-        guests: getRandomElement(GUESTS_MIN_VALUE, GUESTS_MAX_VALUE),
-        checkin: ARRIVAL_TIMES[getRandomElement(0, ARRIVAL_TIMES.length)],
-        checkout: DEPARTURE_TIMES[getRandomElement(0, DEPARTURE_TIMES.length)],
-        features: createDiffArrayLength(FEATURES),
-        description: '',
-        photos: createMixArray(PHOTOS)
-      },
-
-      location: {
-        x: xPoint,
-        y: yPoint
-      }
-    };
-    arrayAdverts.push(objectAdvert);
-  }
-  return arrayAdverts;
-};
-
-var adverts = generateAdverts(AMOUNT_ADVERTS);
 
 // Создание списка фотографий в объявлении
 var renderPhotoAdvert = function (arrayPhotos) {
@@ -208,7 +119,7 @@ var onButtonMainPinMouseUp = function () {
   globalMap.classList.remove('map--faded');
   // Разблокирует поля формы
   window.formAd.classList.remove('ad-form--disabled');
-  mapPinsElement.appendChild(createPins(adverts));
+  mapPinsElement.appendChild(createPins(window.generateAdverts.adverts));
   // Присваивает адрес главной метке(карта активна)
   assignAddressMapPin(false);
   mapMainPin.removeEventListener('mouseup', onButtonMainPinMouseUp);
@@ -225,7 +136,7 @@ var onButtonRandomPinClick = function (evt) {
     return;
   }
   var randomCard = getGeneratedCard(
-      adverts[getIndexNode(documentNode) - AUXILIARY_ELEMENTS_COUNT]
+      window.generateAdverts.adverts[getIndexNode(documentNode) - AUXILIARY_ELEMENTS_COUNT]
   );
   var popupCardClose = randomCard.querySelector('.popup__close');
 
@@ -280,8 +191,8 @@ mapMainPin.addEventListener('mousedown', function (evt) {
       y: mapMainPin.offsetTop - shift.y,
       x: mapMainPin.offsetLeft - shift.x
     };
-    if ((LOCATION_Y_MIN - mapMainPin.offsetHeight <= newMainPinCoords.y) &&
-        (newMainPinCoords.y <= LOCATION_Y_MAX - mapMainPin.offsetHeight)) {
+    if ((window.LOCATION_Y_MIN - mapMainPin.offsetHeight <= newMainPinCoords.y) &&
+        (newMainPinCoords.y <= window.LOCATION_Y_MAX - mapMainPin.offsetHeight)) {
       mapMainPin.style.top = newMainPinCoords.y + 'px';
     }
     if ((globalMap.style.left - (mapMainPin.offsetWidth / 2) <= newMainPinCoords.x) &&
