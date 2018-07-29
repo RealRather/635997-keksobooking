9675eb88cc992fb1dbf76139d8539c75b71dbbdc
RealRather <36735076+RealRather@users.noreply.github.com>
2018-07-29 23:51:56 +0300

    Правит фильтры

--- js/filters.js
+++ js/filters.js
@@ -5,6 +5,8 @@
   var filterHousingType = mapFilters.querySelector('#housing-type');
   var filterHousingPrice = mapFilters.querySelector('#housing-price');
   var filterHousingRooms = mapFilters.querySelector('#housing-rooms');
+  var filterHousingGuests = mapFilters.querySelector('#housing-guests');
+  var filterHousingFeatures = mapFilters.querySelector('#housing-features');
 
   var filterType = function (arrayData) {
     return (filterHousingType.value === 'any' || arrayData.offer.type === filterHousingType.value);
@@ -27,8 +29,22 @@
     return ((arrayData.offer.rooms.toString() === filterHousingRooms.value) || (filterHousingRooms.value === 'any'));
   };
 
+  var filterGuests = function (arrayData) {
+    return ((filterHousingGuests.value === arrayData.offer.guests.toString()) || (filterHousingGuests.value === 'any'));
+  };
+
+  var filterFeatures = function (arrayData) {
+    var checked = filterHousingFeatures.querySelectorAll('input[type=checkbox]:checked');
+    var selected = [].map.call(checked, function (it) {
+      return it.value;
+    });
+    return selected.every(function (feature) {
+      return arrayData.offer.features.includes(feature);
+    });
+  };
+
   var onFormFilterChange = function () {
-    var filterResultArray = window.arrayData.filter(filterType).filter(filterPrice).filter(filterRooms).filter(filterRooms);
+    var filterResultArray = window.arrayData.filter(filterType).filter(filterPrice).filter(filterRooms).filter(filterRooms).filter(filterGuests).filter(filterFeatures);
     window.pins.removeAllPins();
     window.map.closeCard();
     window.pins.renderMapPins(
